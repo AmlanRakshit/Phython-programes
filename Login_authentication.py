b6d524b7cfe436bbfa63d5797ebf8cf6476819f8
AmlanRakshit <arakshit4@gmail.com>
2024-11-04 08:23:00 +0530

    Add files via upload

---
+++ Login_authentication.py
@@ -0,0 +1,43 @@
+# #Log in authentication
+
+# # username = Amlan
+# # password = 00000
+
+
+# # user_name = input ("Enter your name:  ")
+
+# # password = input ("Enter your password:  ")
+
+# # if user_name == "Amlan":
+# #     if password == "125":
+# #         print("Opening File")
+# #     else:
+# #         print ("incorrect password" )
+# # else:
+
+# #     print ("Invalid credential")
+
+
+# #Admission eligibility
+
+# # math >= 65
+# # physics >=55
+# # chemistry >= 50
+ 
+# # Eligibility criteria is m + p + c >= 180
+# # or 
+# # m + p >= 140
+
+# # input is all three subjects
+
+# math_score = int (input ("Enter math score:  "))
+# physics_score = int (input ("Enter physics score:  "))
+# chemistry_score = int (input ("Enter chemistry score:  "))
+
+# total = (math_score + physics_score + chemistry_score)
+
+
+# if (math_score >= 65 and physics_score >=55 and chemistry_score >= 50 and total >=180) or math_score + physics_score >= 140:
+#     print ("you are eligible for admission")
+
+# else: print ("you are not eligible") 
